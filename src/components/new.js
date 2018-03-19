af25af84af23de05ab4a76ee3f5351b231be82f1
Unknown <kalambik@gmail.com>
2018-03-19 19:21:53 +0100

    xx

--- src/components/new.js
+++ src/components/new.js
@@ -22,7 +22,7 @@ class NewInput extends Component {
                 max={10}
                 multiple={false} 
                 onChange={this.props.change} />
-              <i className="far fa-angle-down">
+              <i className="far fa-angle-down"></i>
               <div className="aff">{this.props.$+" "+this.props.devise}</div>
             </div>
 	    );
